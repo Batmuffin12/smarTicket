fc0e4b03fe388d93f335828b550c658ebabe901f
Ofek Bs <ofekbensimhon.work@gmail.com>
2022-11-02 00:00:32 +0200

    server running but login and register isnt working

--- backend/playground/utils/serverUtils.js
+++ backend/playground/utils/serverUtils.js
@@ -2,6 +2,7 @@ const validator = require("validator");
 
 const isEmailValid = (email) => validator.isEmail(email);
 const isCreditCard = (creditCard) => validator.isCreditCard(creditCard);
+
 module.exports = {
   isEmailValid,
   isCreditCard,
