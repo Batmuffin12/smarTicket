2090ef226b7688091228c902132fd8e7c888193d
Ofek Bs <ofekbensimhon.work@gmail.com>
2023-04-30 20:14:32 +0300

    added funcs

--- backend/src/face_recognition/landmark_detection.js
+++ backend/src/face_recognition/landmark_detection.js
@@ -10,3 +10,39 @@ const setup = async () =>
   ]);
 
 setup().then(() => console.log("models loaded successfully"));
+
+function base64ToCanvas(base64) {
+  var image = new Image();
+  image.src = base64;
+
+  var canvas = new Canvas.Canvas(image.width, image.height);
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0, image.width, image.height);
+
+  return canvas;
+}
+
+function detectLandmarks(base64) {
+  var canvas = base64ToCanvas(base64);
+  var displaySize = { width: canvas.width, height: canvas.height };
+
+  return faceapi
+    .detectAllFaces(canvas, new faceapi.SsdMobilenetv1Options())
+    .withFaceLandmarks()
+    .then(function (detections) {
+      var resizedDetections = faceapi.resizeResults(detections, displaySize);
+      return resizedDetections.map(function (d) {
+        return d.landmarks;
+      });
+    });
+}
+
+var base64String = "data:image/jpeg;base64,..."; // Replace with your base64 string URI
+
+detectLandmarks(base64String)
+  .then(function (landmarks) {
+    console.log("Landmarks:", landmarks);
+  })
+  .catch(function (err) {
+    console.error("Error:", err);
+  });
